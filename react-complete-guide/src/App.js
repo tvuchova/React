6f9339eb1e2c3bef894899c235e0609df3dfa91d
Temenuzhka Vuchova <tvuchova@gmail.com>
2020-03-20 23:45:32 +0200

    Initial commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {name:'Max',age:28},
+      {name:'Manu',age:29},
+      {name:'Stef',age:26}
+    ],
+    otherState:'some other value'
+  }
+  switchNameHandler =() =>{
+    this.setState({
+      persons: [
+        {name:'Maximilian',age:28},
+        {name:'Manu',age:29},
+        {name:'Stefanie',age:27}
+      ]
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p>This is really working</p>
+          <button onClick={this.switchNameHandler}>Switch name</button>
+          <Person name ={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}>My hobies:Racing </Person>
+          <Person name = {this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+}
+
+export default App;
